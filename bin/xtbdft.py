eba680b1835136fbfec0f4942ba4c4bdffe509eb
sibo <siboster@gmail.com>
2022-06-07 08:41:41 -0400

    Update xtbdft.py

--- bin/xtbdft.py
+++ bin/xtbdft.py
@@ -4,7 +4,7 @@
 #
 import os, sys, subprocess, time, math
 from datetime import datetime
-goodvibesPy = "~/xtbdft/goodvibes-3.0.1/goodvibes/GoodVibes.py"
+goodvibesPy = "~/xtbdft/GoodVibes/goodvibes/GoodVibes.py"
 xtbPath="~/xtbdft/xtb" # xtb and crest binaries must be located in xtbPath/bin/
 default_cutoff = 3.0
 default_xc = "b3lyp,,b3lyp,b3lyp"
@@ -21,13 +21,13 @@ msubHeader="""#!/bin/bash
 """.format(NP)
 
 ### do not change below code unless you know what you're doing!
-if not (os.path.exists(goodvibesPy)):
+if not (os.path.exists(os.path.exapnduser(goodvibesPy))):
     print("Error: GoodVibes.py does not exist at " + goodvibesPy + "\n Please modify line 7 of xtbdft.py to reflect where GoodVibes.py is located.\nExiting...")
     exit()
-if not (os.path.exists(xtbPath + "/bin/xtb")):
+if not (os.path.exists(os.path.exapnduser(xtbPath + "/bin/xtb"))):
     print("Error: xtb does not exist at " + xtbPath + "/bin/xtb \n Please modify line 8 of xtbdft.py to reflect where xtb is located.\nExiting...")
     exit()
-if not (os.path.exists(xtbPath + "/bin/crest")):
+if not (os.path.exists(os.path.exapnduser(xtbPath + "/bin/crest"))):
     print("Error: crest does not exist at " + xtbPath + "/bin/crest \n Please modify line 8 of xtbdft.py to reflect where crest is located.\nExiting...")
     exit()
 
