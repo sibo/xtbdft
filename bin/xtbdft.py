ce245b5305cf4c5e8e491409006957c05032145e
sibo <siboster@gmail.com>
2022-06-07 08:34:18 -0400

    Update xtbdft.py

--- bin/xtbdft.py
+++ bin/xtbdft.py
@@ -21,13 +21,13 @@ msubHeader="""#!/bin/bash
 """.format(NP)
 
 ### do not change below code unless you know what you're doing!
-if !(os.path.exists(goodvibesPy)):
+if not (os.path.exists(goodvibesPy)):
     print("Error: GoodVibes.py does not exist at " + goodvibesPy + "\n Please modify line 7 of xtbdft.py to reflect where GoodVibes.py is located.\nExiting...")
     exit()
-if !(os.path.exists(xtbPath + "/bin/xtb")):
+if not (os.path.exists(xtbPath + "/bin/xtb")):
     print("Error: xtb does not exist at " + xtbPath + "/bin/xtb \n Please modify line 8 of xtbdft.py to reflect where xtb is located.\nExiting...")
     exit()
-if !(os.path.exists(xtbPath + "/bin/crest")):
+if not (os.path.exists(xtbPath + "/bin/crest")):
     print("Error: crest does not exist at " + xtbPath + "/bin/crest \n Please modify line 8 of xtbdft.py to reflect where crest is located.\nExiting...")
     exit()
 
